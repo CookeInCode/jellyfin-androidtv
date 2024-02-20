16f0767a3e65443046eb6a134e0dba79c4ebe084
CookeInCode <stephencooke.c@gmail.com>
2024-02-19 22:38:49 -0500

    Set preferences for homesection.

--- app/src/main/java/org/jellyfin/androidtvc/preference/UserSettingPreferences.kt
+++ app/src/main/java/org/jellyfin/androidtvc/preference/UserSettingPreferences.kt
@@ -19,11 +19,11 @@ class UserSettingPreferences(
 
 		val homesection0 = enumPreference("homesection0", HomeSectionType.LIBRARY_TILES_SMALL)
 		val homesection1 = enumPreference("homesection1", HomeSectionType.RESUME)
-		val homesection2 = enumPreference("homesection2", HomeSectionType.RESUME_AUDIO)
-		val homesection3 = enumPreference("homesection3", HomeSectionType.RESUME_BOOK)
-		val homesection4 = enumPreference("homesection4", HomeSectionType.LIVE_TV)
-		val homesection5 = enumPreference("homesection5", HomeSectionType.NEXT_UP)
-		val homesection6 = enumPreference("homesection6", HomeSectionType.LATEST_MEDIA)
+		val homesection2 = enumPreference("homesection2", HomeSectionType.LATEST_MEDIA)
+		val homesection3 = enumPreference("homesection3", HomeSectionType.NONE)
+		val homesection4 = enumPreference("homesection4", HomeSectionType.NONE)
+		val homesection5 = enumPreference("homesection5", HomeSectionType.NONE)
+		val homesection6 = enumPreference("homesection6", HomeSectionType.NONE)
 	}
 
 	val homesections
